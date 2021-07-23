a47e0322ca8f326c6cffd1cb0800c0ff24c5f7fd
Helfrench <helfrench.s02@gmail.com>
2021-07-23 22:47:52 +0800

    Change the Logo and the add button

--- index.php
+++ index.php
@@ -15,13 +15,13 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 <script type="text/javascript" src="./js/main.js"></script>
-    <title>Inventory Management System</title>
+    <title>CRUD System</title>
   </head>
 
   <!--NAVBAR-->
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Muhen Travel and Tours</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -69,7 +69,7 @@ generalFunction();
           <input class="form-control" type="text" name="items" placeholder="Enter the list to do...." required/>
         </div>
         <div class="col-md-2">
-          <input class="btn btn-success" type="submit" value="Add Task"/>
+          <input class="btn btn-success" type="submit" value="Add Lang"/>
         </div>
       </div>
   </form> 
