ca00649aff89900eb9cd7543240dcd8f39dbac2a
Helfrench <helfrench.s02@gmail.com>
2021-07-23 23:38:39 +0800

    Created Jumbotron for Website

--- index.php
+++ index.php
@@ -15,13 +15,13 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 <script type="text/javascript" src="./js/main.js"></script>
-    <title>CRUD System</title>
+    <title>Inventory Management System</title>
   </head>
 
   <!--NAVBAR-->
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">Muhen Travel and Tours</a>
+    <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -43,6 +43,11 @@
       
     </div>
 </nav>
+<div class="container">
+<div class="jumbotron">
+  <h1>First branch Jumbotron</h1>
+</div>
+</div>
 
 
 
