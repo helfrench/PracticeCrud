80313acfc9ae85a45b4cd4c385595550e2dfc839
Helfrench <helfrench.s02@gmail.com>
2021-07-23 23:52:28 +0800

    add cards inside the website

--- index.php
+++ index.php
@@ -45,6 +45,37 @@
 </nav>
 
 
+<div class="container">
+    <div class="row mt-5">
+      <div class="col-md card mr-3">
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>  
+      </div>
+
+      <div class="col-md card mr-3"  >
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>  
+      </div>
+
+      <div class="col-md card mr-3"  >
+        <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>  
+      </div>
+    </div>
+</div>
+
 
 <!-- END OF NAVBAR-->
 
